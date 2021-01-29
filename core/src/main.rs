dd3d433ff2d87e2912622f51ae5b0a49effffd6f
Raphael Tang <raphpb1912@gmail.com>
2021-01-29 17:00:26 +0800

    Add more stuff

--- core/src/main.rs
+++ core/src/main.rs
@@ -7,8 +7,9 @@ use util::*;
 use clap::{App, Arg};
 use clap_v3 as clap;
 use once_cell::sync::Lazy;
-use pgp::{types::SecretKeyTrait, Deserializable, SignedSecretKey};
+use sequoia_openpgp::{parse::Parse, Cert};
 use std::{
+    env,
     error::Error,
     fs,
     io::prelude::*,
@@ -18,8 +19,14 @@ use std::{
 
 const NAME: &'static str = env!("CARGO_PKG_NAME");
 
+// static TEMP_DIR: Lazy<Mutex<PathBuf>> = Lazy::new(|| {
+//     let mut path = dirs::cache_dir().unwrap();
+//     path.push(NAME);
+//     Mutex::new(path)
+// });
+
 static TEMP_DIR: Lazy<Mutex<PathBuf>> = Lazy::new(|| {
-    let mut path = dirs::cache_dir().unwrap();
+    let mut path = env::current_dir().unwrap();
     path.push(NAME);
     Mutex::new(path)
 });
@@ -29,6 +36,12 @@ static DATA_DIR: Lazy<Mutex<PathBuf>> = Lazy::new(|| {
     path.push(NAME);
     Mutex::new(path)
 });
+
+// static DATA_DIR: Lazy<Mutex<PathBuf>> = Lazy::new(|| {
+//     let mut path = env::current_dir().unwrap();
+//     path.push("");
+//     Mutex::new(path)
+// });
 
 static PRIVATE_KEY_PATH: Lazy<Mutex<PathBuf>> = Lazy::new(|| {
     let mut path = PathBuf::from(DATA_DIR.lock().unwrap().clone());
@@ -49,6 +62,14 @@ static CHECKSUM_PATH: Lazy<Mutex<PathBuf>> = Lazy::new(|| {
 });
 
 fn main() -> Result<(), Box<dyn Error>> {
+    // Create directories if they don't exist
+    if !TEMP_DIR.lock()?.exists() {
+        fs::create_dir_all(TEMP_DIR.lock()?.clone())?;
+    }
+    if !DATA_DIR.lock()?.exists() {
+        fs::create_dir_all(DATA_DIR.lock()?.clone())?;
+    }
+
     fern::Dispatch::new()
         .format(|out, message, record| {
             out.finish(format_args!(
@@ -79,14 +100,6 @@ fn main() -> Result<(), Box<dyn Error>> {
         )
         .get_matches();
 
-    // Create directories if they don't exist
-    if !TEMP_DIR.lock()?.exists() {
-        fs::create_dir_all(TEMP_DIR.lock()?.clone())?;
-    }
-    if !DATA_DIR.lock()?.exists() {
-        fs::create_dir_all(DATA_DIR.lock()?.clone())?;
-    }
-
     if let Some(input) = args.value_of("INPUT") {
         let input_path = Path::new(input);
 
@@ -94,11 +107,14 @@ fn main() -> Result<(), Box<dyn Error>> {
             let full_path = fs::canonicalize(input_path)?;
             log::debug!("Full input path: {:?}", full_path);
 
+            let file_name = full_path.file_name().unwrap().to_str().unwrap();
+            log::debug!("File name: {}", file_name);
+
             if !args.is_present("decrypt") {
                 // Encrypt
 
                 let mut archive_path = TEMP_DIR.lock()?.clone();
-                archive_path.push(format!("{}.tar", input));
+                archive_path.push(format!("{}.tar", file_name));
 
                 if input_path.is_dir() {
                     log::info!("Archiving folder...");
@@ -116,24 +132,16 @@ fn main() -> Result<(), Box<dyn Error>> {
                 let encrypted_file_path = Path::new(&encrypted_file_path);
 
                 // generate key if not found
-                let (secret_key, public_key) = if !PRIVATE_KEY_PATH.lock()?.clone().exists() {
+                let pgp_key = if !PRIVATE_KEY_PATH.lock()?.clone().exists() {
                     log::info!("Generating PGP key...");
                     generate_key()?
                 } else {
                     log::info!("PGP key already exists, using existing key...");
-                    let secret_key_bytes = fs::read(PRIVATE_KEY_PATH.lock()?.clone())?;
-                    let secret_key = SignedSecretKey::from_bytes(&secret_key_bytes[..])?;
-                    let public_key = secret_key.public_key();
-                    (secret_key, public_key)
+                    Cert::from_file(PRIVATE_KEY_PATH.lock()?.clone())?
                 };
 
                 log::info!("Encrypting file...");
-                encrypt_file(
-                    compressed_archive_path,
-                    encrypted_file_path,
-                    secret_key,
-                    public_key,
-                )?;
+                encrypt_file(compressed_archive_path, encrypted_file_path, pgp_key)?;
 
                 log::info!("Applying checksum...");
                 let checksum = apply_checksum(encrypted_file_path)?;
@@ -142,8 +150,14 @@ fn main() -> Result<(), Box<dyn Error>> {
                 if !checksum_verified {
                     panic!("Checksum not valid!");
                 }
+                log::info!("Checksum verified!");
+
                 let checksum_file_name = encrypted_file_path.file_name().unwrap().to_str().unwrap();
                 let checksum_map_line = format!("{}={}", checksum_file_name, &checksum);
+
+                if !CHECKSUM_PATH.lock()?.clone().exists() {
+                    fs::write(CHECKSUM_PATH.lock()?.clone(), "")?;
+                }
 
                 let mut checksum_file = fs::OpenOptions::new()
                     .write(true)
@@ -151,23 +165,51 @@ fn main() -> Result<(), Box<dyn Error>> {
                     .open(CHECKSUM_PATH.lock()?.clone())?;
 
                 writeln!(checksum_file, "{}", checksum_map_line)?;
+
+                log::info!("Done!");
             } else {
                 // Decrypt
 
-                let file_name = full_path.file_name().unwrap().to_str().unwrap();
-                log::debug!("File name: {}", file_name);
-
+                log::info!("Verifying checksum...");
                 let checksum_list = fs::read_to_string(CHECKSUM_PATH.lock()?.clone())?;
                 log::debug!("Checksum list: {}", checksum_list);
                 let checksum = checksum_list
                     .split("\n")
                     .find(|&x| x.split("=").collect::<Vec<&str>>()[0] == file_name)
-                    .unwrap();
+                    .unwrap()
+                    .split("=")
+                    .collect::<Vec<&str>>()[1];
                 log::debug!("Checksum: {}", checksum);
                 let checksum_verified = verify_checksum(&full_path, &String::from(checksum))?;
                 if !checksum_verified {
                     panic!("Checksum not valid!");
                 }
+
+                log::info!("Reading existing PGP key...");
+                let key = Cert::from_file(PRIVATE_KEY_PATH.lock()?.clone())?;
+
+                let decrypted_file_path_str = full_path.to_string_lossy().replace(".pgp", "");
+                let decrypted_file_path = Path::new(&decrypted_file_path_str);
+
+                log::info!("Decrypting file...");
+                decrypt_file(&full_path, decrypted_file_path, key)?;
+
+                let uncompressed_file_path_str = decrypted_file_path_str.replace(".gz", "");
+                let uncompressed_file_path = Path::new(&uncompressed_file_path_str);
+
+                log::info!("Uncompressing archive/file...");
+                uncompress_file(decrypted_file_path, uncompressed_file_path)?;
+
+                if uncompressed_file_path_str.contains(".tar") {
+                    log::info!("File is archive");
+                    log::info!("Unarchiving file...");
+
+                    let output_path_str = uncompressed_file_path_str.replace(".tar", "");
+                    let output_path = Path::new(&output_path_str);
+                    unarchive_folder(uncompressed_file_path, output_path)?;
+                }
+
+                log::info!("Done!");
             }
         } else {
             panic!("Input path doesn't exist!");
--- core/src/util.rs
+++ core/src/util.rs
@@ -1,41 +1,54 @@
 use io::Cursor;
 use libflate::gzip;
-use pgp::{
-    composed::{key::SecretKeyParamsBuilder, KeyType},
-    crypto::{hash::HashAlgorithm, sym::SymmetricKeyAlgorithm, PublicKeyAlgorithm},
-    packet,
-    ser::Serialize,
-    types::{CompressionAlgorithm, KeyTrait, PublicKeyTrait, SecretKeyTrait},
-    PublicKey, Signature, SignedSecretKey,
+use pgp::{crypto::KeyPair, serialize::Serialize};
+// use pgp::{
+//     composed::{key::SecretKeyParamsBuilder, KeyType},
+//     crypto::{hash::HashAlgorithm, sym::SymmetricKeyAlgorithm, PublicKeyAlgorithm},
+//     packet,
+//     ser::Serialize,
+//     types::{CompressionAlgorithm, KeyTrait, PublicKeyTrait, SecretKeyTrait},
+//     PublicKey, Signature, SignedSecretKey,
+// };
+use sequoia_openpgp as pgp;
+use sequoia_openpgp::{
+    cert::prelude::*,
+    crypto::SessionKey,
+    parse::{stream::*, Parse},
+    policy::{Policy, StandardPolicy},
+    serialize::stream::*,
+    types::SymmetricAlgorithm,
 };
-use sha2::{Digest, Sha256};
-use smallvec::smallvec;
 use std::{
+    collections::HashMap,
     error::Error,
     fs::{self, OpenOptions},
-    io,
+    io::{self, Write},
     path::Path,
 };
-use tar::Builder;
 use walkdir::WalkDir;
 
 use crate::{SileoError, PRIVATE_KEY_PATH};
 
 pub fn archive_folder(folder: &Path, outpath: &Path) -> Result<(), Box<dyn Error>> {
     if folder.is_dir() {
-        if !outpath.exists() {
-            fs::write(outpath, "")?
-        }
+        if !outpath.is_dir() {
+            if !outpath.exists() {
+                log::debug!("Output file does not exist, creating...");
+                fs::write(outpath, "")?;
+            }
 
-        let outfile = OpenOptions::new()
-            .append(true)
-            .read(true)
-            .write(true)
-            .open(outpath)?;
-        let mut archive = Builder::new(outfile);
+            let outfile = OpenOptions::new()
+                .append(true)
+                .read(true)
+                .write(true)
+                .open(outpath)?;
+            let mut archive = tar::Builder::new(outfile);
 
-        for entry in WalkDir::new(folder) {
-            archive.append_path(entry?.path())?;
+            for entry in WalkDir::new(folder) {
+                archive.append_path(entry?.path())?;
+            }
+        } else {
+            return Err(SileoError::new("Output path exists and is a directory!"));
         }
     } else {
         return Err(SileoError::new("Input path is not a directory!"));
@@ -56,103 +69,65 @@ pub fn compress_file(filepath: &Path, outpath: &Path) -> Result<(), Box<dyn Erro
     Ok(())
 }
 
-pub fn encrypt_file(
-    filepath: &Path,
-    outpath: &Path,
-    signing_key: SignedSecretKey,
-    verification_key: PublicKey,
-) -> Result<(), Box<dyn Error>> {
+pub fn encrypt_file(filepath: &Path, outpath: &Path, key: Cert) -> Result<(), Box<dyn Error>> {
     let input_data = fs::read(filepath)?;
 
-    let encrypted_data = encrypt_data(input_data, signing_key, verification_key)?;
+    let encrypted_data = encrypt_data(input_data, &key)?;
 
     fs::write(outpath, encrypted_data)?;
 
     Ok(())
 }
 
-pub fn encrypt_data(
-    data: Vec<u8>,
-    signing_key: SignedSecretKey,
-    verification_key: PublicKey,
-) -> Result<Vec<u8>, Box<dyn Error>> {
-    let now = chrono::Utc::now();
-    let passwd_fn = || String::new();
+pub fn encrypt_data(data: Vec<u8>, key: &Cert) -> Result<Vec<u8>, Box<dyn Error>> {
+    let p = &StandardPolicy::new();
 
-    let digest = {
-        let mut hasher = Sha256::new();
-        hasher.update(data);
-        hasher.finalize()
-    };
-    let digest = digest.as_slice();
+    let encrypted = Vec::new();
 
-    // creates the cryptographic core of the signature without any metadata
-    let signature = signing_key
-        .create_signature(passwd_fn, HashAlgorithm::SHA2_256, digest)
-        .expect("Failed to crate signature");
+    let recipients = key
+        .keys()
+        .with_policy(p, None)
+        .supported()
+        .alive()
+        .revoked(false)
+        .for_transport_encryption();
 
-    // the signature can already be verified
-    verification_key
-        .verify_signature(HashAlgorithm::SHA2_256, digest, &signature)
-        .expect("Failed to validate signature");
+    // Start streaming an pgp message.
+    let message = Message::new(encrypted);
 
-    // wraps the signature in the apropriate package fmt ready to be serialized
-    let signature = Signature::new(
-        pgp::types::Version::Old,
-        packet::SignatureVersion::V4,
-        packet::SignatureType::Binary,
-        PublicKeyAlgorithm::RSA,
-        HashAlgorithm::SHA2_256,
-        [digest[0], digest[1]],
-        signature,
-        vec![
-            packet::Subpacket::SignatureCreationTime(now),
-            packet::Subpacket::Issuer(signing_key.key_id()),
-        ],
-        vec![],
-    );
+    // We want to encrypt a literal data packet.
+    let message = Encryptor::for_recipients(message, recipients).build()?;
 
-    // sign and and write the package (the package written here is NOT rfc4880 compliant)
-    let mut signature_bytes = Vec::with_capacity(1024);
+    // Emit a literal data packet.
+    let mut message = LiteralWriter::new(message).build()?;
 
-    let mut buff = Cursor::new(&mut signature_bytes);
-    packet::write_packet(&mut buff, &signature).expect("Write must succeed");
+    // Encrypt the data.
+    message.write_all(&data)?;
 
-    let raw_signature = signature.signature;
-    verification_key
-        .verify_signature(HashAlgorithm::SHA2_256, digest, &raw_signature)
-        .expect("Verify must succeed");
+    // Finalize the pgp message to make sure that all data is
+    // written.
+    message.finalize()?;
 
-    Ok(signature_bytes)
+    Ok(data)
 }
 
-pub fn generate_key() -> Result<(SignedSecretKey, PublicKey), Box<dyn Error>> {
-    let mut key_params = SecretKeyParamsBuilder::default();
-    key_params
-        .key_type(KeyType::Rsa(2048))
-        .can_create_certificates(false)
-        .can_sign(true)
-        .primary_user_id("Me <me@example.com>".into())
-        .preferred_symmetric_algorithms(smallvec![SymmetricKeyAlgorithm::AES256])
-        .preferred_hash_algorithms(smallvec![HashAlgorithm::SHA2_256])
-        .preferred_compression_algorithms(smallvec![CompressionAlgorithm::ZLIB]);
-    let secret_key_params = key_params
-        .build()
-        .expect("Must be able to create secret key params");
-    let secret_key = secret_key_params
-        .generate()
-        .expect("Failed to generate a plain key.");
-    let passwd_fn = || String::new();
-    let signed_secret_key = secret_key
-        .sign(passwd_fn)
-        .expect("Must be able to sign its own metadata");
-    let public_key = signed_secret_key.public_key();
+/// Generates a signing key
+pub fn generate_key() -> Result<Cert, Box<dyn Error>> {
+    let (cert, _revokation) = CertBuilder::new()
+        .add_userid("someone@example.org")
+        .add_transport_encryption_subkey()
+        .generate()?;
 
-    let signed_secret_key_bytes = signed_secret_key.to_bytes()?;
+    if !PRIVATE_KEY_PATH.lock()?.exists() {
+        fs::write(PRIVATE_KEY_PATH.lock()?.clone(), "")?;
+        let mut key_file = OpenOptions::new()
+            .write(true)
+            .open(PRIVATE_KEY_PATH.lock()?.clone())?;
 
-    fs::write(PRIVATE_KEY_PATH.lock()?.clone(), signed_secret_key_bytes)?;
+        cert.export(&mut key_file)?;
+    }
 
-    Ok((signed_secret_key, public_key))
+    Ok(cert)
 }
 
 pub fn apply_checksum(filepath: &Path) -> Result<String, Box<dyn Error>> {
@@ -175,4 +150,145 @@ pub fn verify_checksum(filepath: &Path, checksum: &String) -> Result<bool, Box<d
     } else {
         Ok(false)
     }
+}
+
+pub fn decrypt_file(filepath: &Path, outpath: &Path, key: Cert) -> Result<(), Box<dyn Error>> {
+    let encrypted_data = fs::read(filepath)?;
+
+    let decrypted_data = decrypt_data(encrypted_data, key)?;
+
+    fs::write(outpath, decrypted_data)?;
+
+    Ok(())
+}
+
+pub fn decrypt_data(data: Vec<u8>, key: Cert) -> Result<Vec<u8>, Box<dyn Error>> {
+    let p = &StandardPolicy::new();
+
+    let mut decrypted = Vec::new();
+
+    // Now, create a decryptor with a helper using the given Certs.
+    let mut decryptor =
+        DecryptorBuilder::from_bytes(&data)?.with_policy(p, None, Helper::new(p, vec![key]))?;
+
+    // Decrypt the data.
+    io::copy(&mut decryptor, &mut decrypted)?;
+
+    Ok(decrypted)
+}
+
+/// This helper provides secrets for the decryption, fetches public
+/// keys for the signature verification and implements the
+/// verification policy.
+struct Helper {
+    keys: HashMap<pgp::KeyID, KeyPair>,
+}
+
+impl Helper {
+    /// Creates a Helper for the given Certs with appropriate secrets.
+    fn new(p: &dyn Policy, certs: Vec<pgp::Cert>) -> Self {
+        // Map (sub)KeyIDs to secrets.
+        let mut keys = HashMap::new();
+        for cert in certs {
+            for ka in cert
+                .keys()
+                .unencrypted_secret()
+                .with_policy(p, None)
+                .for_storage_encryption()
+                .for_transport_encryption()
+            {
+                keys.insert(ka.key().keyid(), ka.key().clone().into_keypair().unwrap());
+            }
+        }
+
+        Helper { keys }
+    }
+}
+
+impl DecryptionHelper for Helper {
+    fn decrypt<D>(
+        &mut self,
+        pkesks: &[pgp::packet::PKESK],
+        _skesks: &[pgp::packet::SKESK],
+        sym_algo: Option<SymmetricAlgorithm>,
+        mut decrypt: D,
+    ) -> pgp::Result<Option<pgp::Fingerprint>>
+    where
+        D: FnMut(SymmetricAlgorithm, &SessionKey) -> bool,
+    {
+        // Try each PKESK until we succeed.
+        for pkesk in pkesks {
+            if let Some(pair) = self.keys.get_mut(pkesk.recipient()) {
+                if pkesk
+                    .decrypt(pair, sym_algo)
+                    .map(|(algo, session_key)| decrypt(algo, &session_key))
+                    .unwrap_or(false)
+                {
+                    break;
+                }
+            }
+        }
+        // XXX: In production code, return the Fingerprint of the
+        // recipient's Cert here
+        Ok(None)
+    }
+}
+
+impl VerificationHelper for Helper {
+    fn get_certs(&mut self, _ids: &[pgp::KeyHandle]) -> pgp::Result<Vec<pgp::Cert>> {
+        Ok(Vec::new()) // Feed the Certs to the verifier here.
+    }
+    fn check(&mut self, structure: MessageStructure) -> pgp::Result<()> {
+        for layer in structure.iter() {
+            match layer {
+                MessageLayer::Compression { algo } => eprintln!("Compressed using {}", algo),
+                MessageLayer::Encryption {
+                    sym_algo,
+                    aead_algo,
+                } => {
+                    if let Some(aead_algo) = aead_algo {
+                        eprintln!("Encrypted and protected using {}/{}", sym_algo, aead_algo);
+                    } else {
+                        eprintln!("Encrypted using {}", sym_algo);
+                    }
+                }
+                MessageLayer::SignatureGroup { ref results } => {
+                    for result in results {
+                        match result {
+                            Ok(GoodChecksum { ka, .. }) => {
+                                eprintln!("Good signature from {}", ka.cert());
+                            }
+                            Err(e) => eprintln!("Error: {:?}", e),
+                        }
+                    }
+                }
+            }
+        }
+        Ok(()) // Implement your verification policy here.
+    }
+}
+
+pub fn uncompress_file(filepath: &Path, outpath: &Path) -> Result<(), Box<dyn Error>> {
+    let compressed = Cursor::new(fs::read(filepath)?);
+
+    let mut decoder = gzip::Decoder::new(compressed)?;
+
+    let mut uncompressed = OpenOptions::new().write(true).open(outpath)?;
+
+    io::copy(&mut decoder, &mut uncompressed)?;
+
+    Ok(())
+}
+
+pub fn unarchive_folder(filepath: &Path, outpath: &Path) -> Result<(), Box<dyn Error>> {
+    if !outpath.exists() {
+        fs::create_dir_all(outpath)?;
+    }
+
+    let infile = Cursor::new(fs::read(filepath)?);
+
+    let mut archive = tar::Archive::new(infile);
+    archive.unpack(outpath)?;
+
+    Ok(())
 }
